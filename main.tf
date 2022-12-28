ae3ad89b015226866431d68eb814a06f87d5759e
shiva shankar <sivas1475@gmail.com>
2022-12-28 12:10:49 +0530

    JENKINS

--- main.tf
+++ main.tf
@@ -9,11 +9,8 @@ module "vpc" {
 
 module "rabbitmq" {
   source         = "git::https://github.com/DShankarGoud/tf-module-rabbitmq.git"
-  VPC_CIDR_BLOCK = var.VPC_CIDR_BLOCK
   COMPONENT      = var.COMPONENT
   ENV            = var.ENV
-  SUBNET_CIDR    = var.SUBNET_CIDR
-  AZ             = var.AZ
   VPC_ID         = module.vpc.VPC_ID
   VPC_CIDR       = module.vpc.VPC_CIDR
   SUBNET_IDS     = module.vpc.SUBNET_IDS
